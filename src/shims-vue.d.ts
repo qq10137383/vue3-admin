788e5e37ec839dc8019b1dea2e08f82e92995dea
bubble <389565230@qq.com>
2021-12-28 10:15:22 +0800

    修改shims-vue的注释

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,13 @@
 import { CustomFilters } from "./filters"
 
 /**
- * ts识别vue文件中的导出
+ * 重新定义.vue文件的导出，伪装组件导出的options，使ts识别为组件构造函数实例
+ * DefineComponent定义了组件的构造函数，component即是构造函数实例(DefineComponent实例)
+ * 所以如果需要获取组件类型，可以这样获取
+ * import MyComponent from './my-Component'
+ * type MyComponentType = InstanceType<typeof MyComponent>
+ * 1、typeof MyComponent获取构造函数类型(ComponentPublicInstanceConstructor)
+ * 2、InstanceType获取vue内部生成的组件类型(ComponentPublicInstance)
  */
 /* eslint-disable */
 declare module '*.vue' {
